e938cda55b337b61a3134e176372fc786c604916
ahd3r <ander11110@gmail.com>
2018-11-23 23:17:26 +0200

    done some staff as well

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 document.querySelector('.add').addEventListener('click', addTask);
-document.querySelector('.add').addEventListener('submit', addTask);
-const getTaskFromStorage=localStorage.getItem('tasks');
-const masOfTasks=JSON.parse(getTaskFromStorage);
 
 const noTask=()=>{
-    document.querySelector('#parentForTasks').appendChild(document.createElement('div')).className='row';
-    let arr=document.querySelectorAll('#parentForTasks>div');
-    arr[arr.length - 1].appendChild(document.createElement('div')).className='col';
-    arr[arr.length - 1].querySelector('.col').appendChild(document.createElement('p')).className='text-center';
-    arr[arr.length - 1].querySelector('p').textContent='No task';
-    document.querySelector('#parentForTasks').appendChild(document.createElement('div')).className='row';
-    arr=document.querySelectorAll('#parentForTasks>div');
-    arr[arr.length - 1].appendChild(document.createElement('div')).className='col';
-    arr[arr.length - 1].querySelector('.col').appendChild(document.createElement('div')).className='ot1';
-    arr[arr.length - 1].querySelector('.col').appendChild(document.createElement('button')).className='left btn black waves-effect waves-light disabled';
-    arr[arr.length - 1].querySelector('button').setAttribute('id','del-btn');
-    arr[arr.length - 1].querySelector('button').textContent='delete All';
+    let divForChange=document.querySelector('#forStaff');
+    while(divForChange.firstChild){
+        divForChange.firstChild.remove();
+    }
+    divForChange.appendChild(document.createElement('p')).className='text-center';
+    divForChange.querySelector('p').textContent='No task';
+    document.querySelector('button').className='left btn black waves-effect waves-light disabled';
 };
 
-const yesTask=()=>{
-    document.querySelector('#parentForTasks').appendChild(document.createElement('div')).className='row';
-    let arr=document.querySelectorAll('#parentForTasks>div');
-    arr[arr.length-1].appendChild(document.createElement('div')).className='col';
-    arr[arr.length-1].querySelector('.col').appendChild(document.createElement('ul')).className='collection';
-    masOfTasks.forEach(element => {
+const yesTask=(getMasOfTasksFromStorage)=>{
+    let divForChange=document.querySelector('#forStaff');
+    while(divForChange.firstChild){
+        divForChange.firstChild.remove();
+    }
+    divForChange.appendChild(document.createElement('ul')).className='collection';
+    getMasOfTasksFromStorage.forEach(element => {
         document.querySelector('ul').appendChild(document.createElement('li')).className='collection-item';
         let arr1=document.querySelectorAll('li');
         arr1[arr1.length-1].textContent=element;
@@ -32,44 +25,49 @@ const yesTask=()=>{
         arr1[arr1.length-1].querySelector('i').textContent='clear';
         arr1[arr1.length-1].querySelector('a').addEventListener('click', delOneTask);
     });
-    document.querySelector('#parentForTasks').appendChild(document.createElement('div')).className='row';
-    arr=document.querySelectorAll('#parentForTasks>div');
-    arr[arr.length - 1].appendChild(document.createElement('div')).className='col';
-    arr[arr.length - 1].querySelector('.col').appendChild(document.createElement('div')).className='ot2';
-    arr[arr.length - 1].querySelector('.col').appendChild(document.createElement('button')).className='left btn black waves-effect waves-light';
-    arr[arr.length - 1].querySelector('button').setAttribute('id','del-btn');
-    arr[arr.length - 1].querySelector('button').textContent='delete All';
+    document.querySelector('button').className='left btn black waves-effect waves-light';
+    document.querySelector('#del-btn').addEventListener('click', delAll);
 };
 
-if(getTaskFromStorage===null || getTaskFromStorage==='[]'){
-    localStorage.setItem('tasks', '[]');
-    noTask();
-} else{
-    yesTask();
-}
-
-if(document.querySelector('#del-btn').classList.contains('disabled')===false){
-    document.querySelector('#del-btn').addEventListener('click', delAll);
-}
-
-function addTask(){
-    const writeStaff=document.querySelector('input').value;
-    if(writeStaff!==''){
-        const val = JSON.parse(localStorage.getItem('tasks'));
-        val.push(writeStaff);
-        localStorage.setItem('tasks', JSON.stringify(val));
-        location.reload();
+function checkStart(noTask,yesTask){
+    let getTaskFromStorage=localStorage.getItem('tasks');
+    let masOfTasks=JSON.parse(getTaskFromStorage);
+    if(getTaskFromStorage===null || getTaskFromStorage==='[]'){
+        localStorage.setItem('tasks', '[]');
+        noTask();
+    } else{
+        yesTask(masOfTasks);
     }
+}
+
+checkStart(noTask,yesTask);
+
+function addTask(e){
+    let getTaskFromStorage=localStorage.getItem('tasks');
+    let masOfTasks=JSON.parse(getTaskFromStorage);
+    let writeStaff=document.querySelector('input').value;
+    if(writeStaff!==''){
+        masOfTasks.push(writeStaff);
+        localStorage.setItem('tasks', JSON.stringify(masOfTasks));
+    }
+    checkStart(noTask,yesTask);
 }
 
 function delAll(){
     localStorage.setItem('tasks','[]');
-    location.reload();
+    checkStart(noTask,yesTask);
 }
 
-function delOneTask(e){
+function delOneTask(e){ // add pre-load, clear an input after add task, color
+    let getTaskFromStorage=localStorage.getItem('tasks');
+    let masOfTasks=JSON.parse(getTaskFromStorage);
     let thisTask = e.path[2].textContent;
-    masOfTasks.splice(masOfTasks.indexOf(thisTask.substr(0,thisTask.length-5)),1);
-    localStorage.setItem('tasks', JSON.stringify(masOfTasks));
-    location.reload();
+    let masOfTaskInPage=document.querySelectorAll('li');
+    masOfTaskInPage.forEach((element,index)=>{
+        if(element===e.target.parentElement.parentElement){
+            masOfTasks.splice(index,1);
+            localStorage.setItem('tasks', JSON.stringify(masOfTasks));
+        }
+    })
+    checkStart(noTask,yesTask);
 }
