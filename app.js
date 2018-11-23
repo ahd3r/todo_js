eaa6f869cd6ae1f7bf2f11c3a23f2836032742b8
ahd3r <ander11110@gmail.com>
2018-11-23 23:44:38 +0200

    done some staff

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ const yesTask=(getMasOfTasksFromStorage)=>{
         arr1[arr1.length-1].querySelector('i').textContent='clear';
         arr1[arr1.length-1].querySelector('a').addEventListener('click', delOneTask);
     });
+    let backWallOdd=document.querySelectorAll('li:nth-child(odd)');
+    backWallOdd.forEach(e=>{
+        e.style.background='rgb(230, 230, 230)';
+    });
     document.querySelector('button').className='left btn black waves-effect waves-light';
     document.querySelector('#del-btn').addEventListener('click', delAll);
 };
@@ -58,7 +62,7 @@ function delAll(){
     checkStart(noTask,yesTask);
 }
 
-function delOneTask(e){ // add pre-load, clear an input after add task, color
+function delOneTask(e){ // add pre-load, clear an input after add task
     let getTaskFromStorage=localStorage.getItem('tasks');
     let masOfTasks=JSON.parse(getTaskFromStorage);
     let thisTask = e.path[2].textContent;
